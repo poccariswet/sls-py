8ac6c43ac2c4a235b02279cbd15b2ff0a20acc1f
soeyusuke <s1240231@gmail.com>
2018-05-12 17:48:52 +0900

    [FIX]demo

--- handler.py
+++ handler.py
@@ -3,14 +3,49 @@ import json
 
 def hello(event, context):
     # TODO: event['body']をパースして、idをとり、select
-    body = {
-        "message": "success serverless!!!",
-        "input": event['body']
+    body = event['body']
+    Items = []
+    Item1 = {
+        "Item": {
+            "skucode": "0001002",
+            "商品名": "コーヒー",
+            "9": 4,
+            "11": 7,
+            "12": 9,
+            "13": 8,
+            "14": 4,
+            "15": 1
+        }
     }
+    Item2 = {
+        "Item": {
+            "skucode": "0001002",
+            "商品名": "コーヒー",
+            "9": 4,
+            "11": 7,
+            "12": 9,
+            "13": 8,
+            "14": 4,
+            "15": 1
+        }
+    }
+    Item3 = {
+        "Item": {
+            "skucode": "0001002",
+            "商品名": "コーヒー",
+            "9": 4,
+            "11": 7,
+            "12": 9,
+            "13": 8,
+            "14": 4,
+            "15": 1
+        }
+    }
+    Items = [Item1, Item2, Item3]
 
     response = {
         "statusCode": 200,
-        "body": json.dumps(body)
+        "body": json.dumps(Items)
     }
 
     return response
